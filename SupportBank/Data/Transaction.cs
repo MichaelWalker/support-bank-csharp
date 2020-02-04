a8ea10f2baa203338b58e2a4d21c0c5cfeae67c2
Mike <Walker>
2020-02-04 17:47:50 +0000

    Create a class to hold the transactions.

---
+++ SupportBank/Data/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SupportBank.Data
+{
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public double Amount { get; set; }
+    }
+}
